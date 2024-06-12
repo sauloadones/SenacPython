e5401012e86b9efad366fe805ee8453e097ee94e
adon3s <141533318+sauloadones@users.noreply.github.com>
2024-06-12 11:51:14 -0300

    Update

--- classe.py
+++ classe.py
@@ -1,72 +1,65 @@
 class ContaBancaria:
-    def _init_(bank, id, saldo, titular, depositar, sacar):
-       
-        bank._id = id
-        bank._saldo = saldo
-        bank._titular = titular
-        bank._depositar = depositar
-        bank._sacar = sacar
-       
-    def get_id(bank):
-       
-        return bank._id
+    def __init__(self):
+        self._ids = []
+        self._saldos = []
+        self._titulares = []
+
+    def get_ids(self):
+        return self._ids
         
-    def get_saldo(bank):
-        
-        return bank._saldo
-    def get_titular(bank):
-       
-        return bank._titular
-    def set_id(bank, id):
-       
+    def get_saldos(self):
+        return self._saldos
+    
+    def get_titulares(self):
+        return self._titulares
+    
+    def set_id(self, id):
         if not isinstance(id, int):
             raise ValueError("Valor não é inteiro")
-        bank._id = id
+        self._ids.append(id)
         
+    def set_saldo(self, saldo):
+        if not isinstance(saldo, float):
+            raise ValueError("Valor não é float")
+        self._saldos.append(saldo)
         
-      
-       
-    def set_saldo(bank, saldo):
-        
-            if not isinstance(saldo, float):
-                raise ValueError("Valor não e float")
-            bank._saldo = saldo 
-            
-      
-       
-        
-    def set_titular(bank, titular):
-        
-            if not isinstance(titular, str):
-                raise ValueError("Valor não é string")
-            bank._titular = titular
-            
-            
-        
-    def get_depositar(bank):
-        
-        return bank._depositar
-        
-    def set_depositar(bank, depositar):
-        if not isinstance(depositar, float):
-            raise ValueError("Valor não e float")
-        bank._depositar = depositar 
-        bank._saldo += bank._depositar
-    def get_sacar(bank):
-        bank._saldo -= bank._sacar
-        return bank._sacar
-        
-    def set_sacar(bank, sacar):
-        if not isinstance(sacar, float):
-            raise ValueError("Valor não e float")
-        bank._sacar = sacar
-        bank._saldo -= bank._sacar
+    def set_titular(self, titular):
+        if not isinstance(titular, str):
+            raise ValueError("Valor não é string")
+        self._titulares.append(titular)
     
-       
-
-
-
-
+    def depositar(self, id, valor):
+        if not isinstance(valor, float):
+            raise ValueError("Valor não é float")
+        index = self._ids.index(id)
+        self._saldos[index] += valor
+    
+    def sacar(self, id, valor):
+        if not isinstance(valor, float):
+            raise ValueError("Valor não é float")
+        index = self._ids.index(id)
+        if self._saldos[index] >= valor:
+            self._saldos[index] -= valor
+        else:
+            raise ValueError("Saldo insuficiente")
+    
+    def get_saldo_pelo_id(self, id):
+        index = self._ids.index(id)
+        return self._saldos[index]
+    
+    def get_titular_pelo_id(self, id):
+        index = self._ids.index(id)
+        return self._titulares[index]
+    
+    def get_id_pelo_titular(self, id):
+        index = self._titulares.index(id)
+        return self._ids[index]
+    
+    def get_deletar_conta(self, id):
+        index = self._ids.index(id)
+        self._ids.pop(index)
+        self._titulares.pop(index)
+        self._saldos.pop(index)
     
    
     
--- usuario.py
+++ usuario.py
@@ -1,95 +1,106 @@
 from classe import ContaBancaria
-
+import os
 conta = ContaBancaria()
 
-conta.set_saldo(0.00)
-nomes = []
-ids = []
-saldos = []
-
-# def depositar():
-#    valor =  float(input("Digite o valor para depositar"))
-#    x = conta.get_saldo()
-#    total = x + valor 
-#    conta.set_saldo(total)
-# def sacar():
-#    valor =  float(input("Digite o valor para depositar"))
-#    x = conta.get_saldo()
-#    total = x + valor
-#    conta.set_saldo(total)
 i = 0
 while True:
-   
    print("1 - Depositar na conta")
    print("2 - Sacar da Conta")
    print("3 - Check saldo da conta")
    print("4 - Adicionar uma conta")
-   print("5 - Sair do Menu")
+   print("5 - Para deletar a conta")
+   print("6 - Para verificar o status da conta")
+   print("7 - Para sair")
    
-   op = input("Digite o numero do menu")
+   op = input("Digite o numero do menu: ")
 
    if op == '1':
        try:
-        check = int(input("Digite o id da sua conta"))
-        if check in ids:
-            valor = float(input("Digite o valor a ser digitado"))
-            
-            conta.set_depositar(valor)
-            consulta = ids.index(check)
-            saldos[consulta] = conta.get_saldo()
-
-        else:
-           raise ValueError
+           check = int(input("Digite o id da sua conta: "))
+           if check in conta.get_ids():
+               valor = float(input("Digite o valor para depositar: "))
+               conta.depositar(check, valor)
+               print(f"Bem vindo! {conta.get_titular_by_id(check)}\nDepósito realizado com sucesso!")
+           else:
+               raise ValueError
+           
        except ValueError:
-          print("Sua conta não esta cadastrada")
+           print("Sua conta não está cadastrada ou valor inválido.")
+     
    elif op == '2':
-        try:
-            check = int(input("Digite o id da sua conta"))
-            if check in ids:
-                valor = float(input("Digite o valor a ser digitado"))
-                consulta = ids.index(check)
-                conta.set_sacar(valor)
-                saldos[consulta] = conta.get_saldo()
-            else:
-                raise ValueError
-        except ValueError:
-          print("Sua conta não esta cadastrada")
-       
+       try:
+           check = int(input("Digite o id da sua conta: "))
+           if check in conta.get_ids():
+               valor = float(input("Digite o valor para sacar: "))
+               conta.sacar(check, valor)
+               print(f"Bem vindo!!\n{conta.get_titular_by_id(check)}\nSaque realizado com sucesso!")
+           else:
+               raise ValueError
+       except ValueError:
+           print("Sua conta não está cadastrada ou saldo insuficiente.")
+     
    elif op == '3':
+       try:
+           check = int(input("Digite o id da sua conta: "))
+           if check in conta.get_ids():
+               saldo = conta.get_saldo_by_id(check)
+               print(f"OLA {conta.get_id_by_titulares(check)}!\nSUA CONTA É:{check}!\n Seu atual saldo é{saldo}!")
+           else:
+               raise ValueError
+       except ValueError:
+           print("Sua conta não está cadastrada.")
+    
+   elif op == '4':
+       try:
+           i += 1
+           nome = input("Digite seu nome: ")
+           conta.set_titular(nome)
+           conta.set_id(1000+i)
+           conta.set_saldo(0.0)
+           qlq = conta.get_id_pelo_titular(nome)
+           print(f"O id da sua conta é: {qlq}")
+           os.system('clear')
+       except ValueError:
+           print("Erro ao adicionar a conta, verifique os dados.")
+       
+   elif op == '5':
+       check = int(input("Digite o id da sua conta"))
+       try:
+        if check in conta.get_ids():
+           conta.get_deletar_conta(check)
+           print("Sua conta foi deletada")
+           print(conta.get_ids(), conta.get_titulares(), conta.get_saldos())
+        else:
+            raise ValueError
+       except  ValueError:
+           print("Sua conta nunca existiu ou voce ja deletou!")
+    
+   elif op == '6':
+        check = int(input("Digite o id da sua conta"))
         try:
-            check = int(input("Digite o id da sua conta"))
-            if check in ids:
-              
-              
-              consulta = ids.index(check)
-              print("Seu saldo é", saldos[consulta])
+            if check  not in conta.get_ids():
+                print("Sua conta não existe no nosso sistema")
+            elif check in conta.get_ids():
+                print("Sua conta existe no nosso sistema")
+                nome = conta.get_titular_pelo_id(check)
+                conta = conta.get_id_pelo_titular(nome)
+                os.system('cls')
+               
+                print(f"O nome da sua conta é {nome}\nO codigo bancario é{conta}")
             else:
                 raise ValueError
+                
         except ValueError:
-          print("Sua conta não esta cadastrada")
-      
-   elif op == '4':
-      
-      try:
-        nome = input("Digite seu nome")
-        conta.set_titular(nome)
-        i += 1
-        conta.set_id(i)
-        nomes.append(conta.get_titular())
-        ids.append(conta.get_id())
-        saldos.append(0)
-        print("O id da sua conta é", conta.get_id())
-        
-      except ValueError:
-         print("Tu digitou algo errado ai")
+            print("Digite numeros")
+            
+            
+           
+
     
-   elif op == '5':
-      break
+
+   elif op == '7':
+       break
    
    else:
-      print("Valor errado tente novamente")
-   
+       print("Valor errado, tente novamente.")
     
-
-   
-   
